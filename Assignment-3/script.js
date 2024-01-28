7a37f032828f8edecced04998be530978a47a080
hetalgada15 <114257026+hetalgada15@users.noreply.github.com>
2024-01-28 09:34:46 -0500

    addition of student

--- Assignment-3/script.js
+++ Assignment-3/script.js
@@ -35,3 +35,64 @@ function mycollapse(collapse_row){
     row_no.style.display= "none";
   }
 }
+
+//Addition of students in a table:
+
+function addStudent() {
+
+  var table = document.getElementById("myTable");
+  var l = table.rows.length;
+  console.log(l)
+  var row_no;
+  if (l == 1) {
+    var row_no = 1;
+  }
+  else {
+    var row_no = parseInt(table.rows[l - 2].cells[1].innerHTML.slice(-1)) + 1;
+
+  }
+
+  var row = table.insertRow(-1);
+  row.setAttribute("id", "row_" + row_no);
+
+  row.style.display = 'table-row'
+  var cell;
+  for (var i = 0; i < 8; i++) {
+    cell = row.insertCell(i);
+
+    if (i == 0) {
+      cell.innerHTML = "<input type='checkbox'" + "id='check_" + row_no + "' onclick=\"oncheck('row_" + row_no + "','check_" + row_no + "')\"/><br /><br /><img src='down.png' width='25px' onclick=\"mycollapse('collapse_row_" + row_no + "' )\" />";
+    }
+    else if (i == 1) {
+      cell.innerHTML = "Student " + row_no;
+    }
+    else if (i == 2) {
+      cell.innerHTML = "Teacher  " + row_no;
+    }
+    else if (i == 3) {
+      cell.innerHTML = "Approved";
+    }
+    else if (i == 4) {
+      cell.innerHTML = "Fall";
+    }
+    else if (i == 5) {
+      cell.innerHTML = "TA";
+    }
+    else if (i == 6) {
+      cell.innerHTML = "12345";
+    }
+    else if (i == 7) {
+      cell.innerHTML = "100%";
+    }
+
+  }
+
+  txt = "Student " + row_no + " Record added successfully";
+  window.alert(txt);
+  var hidden_row = table.insertRow(-1);
+  hidden_row.setAttribute("id", "collapse_row_" + row_no);
+  hidden_row.classList.add("dropDownTextArea");
+  hidden_cell = hidden_row.insertCell(0);
+  hidden_row.innerHTML = "<td colspan='8'> Advisor:<br /><br /> Award Details<br /> Summer 1-2014(TA)<br /> Budget Number: <br /> Tuition Number: <br /> Comments:<br /><br /><br /> Award Status:<br /><br /><br /> </td>"
+}
+
