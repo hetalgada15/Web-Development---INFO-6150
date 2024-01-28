169a33b70c41eae4af3cdb127c726895eb60b949
hetalgada15 <114257026+hetalgada15@users.noreply.github.com>
2024-01-28 08:43:02 -0500

    collapsing of files

--- Assignment-3/script.js
+++ Assignment-3/script.js
@@ -16,3 +16,22 @@ var socialMedia = {
 };
 
 var t = new Title("CONNECT WITH ME!");
+var check_flags=0;
+var count_student= 0;
+
+
+//collapsing of rows:
+
+function mycollapse(collapse_row){
+  console.log(document.getElementById(collapse_row).style.display);
+  var row_no= document.getElementById(collapse_row);
+
+  if(row_no.style.display== 'none') //if the row is currently hidden
+  {
+    row_no.style.display= "table-row"; //make it visible
+  }
+
+  else{
+    row_no.style.display= "none";
+  }
+}
